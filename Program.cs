4d7eb5769d27cfffbdfa37c14b135618de31470f
Dmitrij <dmitrij@inbox.lt>
2022-10-13 15:18:42 +0300

    ДЗ 4

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//   Console.Write("Введите число A: ");
+//   int numberA = int.Parse(Console.ReadLine()!);
+//   Console.Write("Введите число B: ");
+//   int numberB = int.Parse(Console.ReadLine()!);
+// int exponentiation = Exponentiation(numberA, numberB);
+//   Console.WriteLine($"{numberA}, {numberB} -> " + exponentiation);
+
+// int Exponentiation(int numberA, int numberB){
+//   int result = 1;
+//   for(int i=1; i <= numberB; i++){
+//     result = result * numberA;
+//   }
+//     return result;
+// }
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Write("Введите число N: ");
+// int numberN = int.Parse(Console.ReadLine()!);
+// int sumNumber = SumNumber(numberN);
+// Console.WriteLine($"{numberN} -> "  + sumNumber);
+
+//   int SumNumber(int numberN){
+    
+//     int counter = Convert.ToString(numberN).Length;
+//     int advance = 0;
+//     int result = 0;
+
+//     for (int i = 0; i < counter; i++){
+//       advance = numberN - numberN % 10;
+//       result = result + (numberN - advance);
+//       numberN = numberN / 10;
+//     }
+//    return result;
+//   }
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
